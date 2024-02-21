20edbf360da047602f5b643f678b1e1171424ee3
Juzbird11 <juzbird11@gmail.com>
2024-02-21 09:39:15 +0700

    set repo for deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,11 @@ import * as React from 'react'
 
 const IndexPage = () => {
     return (
-        <h1>Hello world</h1>
+        <main>
+            <h1>Hello world</h1>
+            <h2>Morning!!!</h2>
+            <h3>How are you?</h3>
+        </main>
     )
 }
 
