0fdcb643d138e9d5cc67871b9f46bf580f06c907
Han Htet Aung <juzbird11@gmail.com>
2024-08-04 11:31:40 +0700

    add card component to projects section

--- src/components/project-link.js
+++ src/components/project-link.js
@@ -1,13 +1,45 @@
-import { Typography } from "@material-tailwind/react";
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+} from "@material-tailwind/react";
+
 import { Link } from "gatsby"
 
 export default function ProjectLink({link, name, technologies}) {
     return (
-        <>
-            <Link to={link}>
-                <Typography variant='h6' className='text-blue-700'>{name}</Typography>
-            </Link>
-            <Typography variant='paragraph'>Technologies: {technologies}</Typography>
-        </>
+        <Card className="rounded-none border-black border-2">
+            <CardBody className="pb-4">
+                <Typography variant="h5" color="blue-gray" className="">
+                {name}
+                </Typography>
+                <Typography className="text-black">
+                Technologies: {technologies}
+                </Typography>
+            </CardBody>
+            <CardFooter className="pt-0">
+                <Link to={link} className="inline-block">
+                <Button size="sm" variant="text" className="flex items-center gap-2 text-blue-600">
+                    See details
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="h-4 w-4"
+                    >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                    />
+                    </svg>
+                </Button>
+                </Link>
+            </CardFooter>
+        </Card>
     );
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -5,20 +5,17 @@ import { Typography } from "@material-tailwind/react";
 const Projects = () => {
     return (
         <section>
-            <Typography variant='h4'>Projects & Experience</Typography>
-            <br/>
+            <Typography variant='h4' className='mb-4'>Projects & Experience</Typography>
 
-            <ProjectLink link='/projects/hospital-management-system' name='Hospital Management System' technologies='Bootstrap, AdminLTE, Laravel, MariaDB' />
-            <br/>
+            <div className='grid lg:grid-cols-2 gap-4'>
+                <ProjectLink link='/projects/hospital-management-system' name='Hospital Management System' technologies='Bootstrap, AdminLTE, Laravel, MariaDB' />
 
-            <ProjectLink link='/projects/cinema' name='Cinema' technologies='React' />
-            <br />
+                <ProjectLink link='/projects/cinema' name='Cinema' technologies='React' />
 
-            <ProjectLink link='/projects/drawing-board' name='Drawing Board' technologies='JavaSE, Swing' />
-            <br />
+                <ProjectLink link='/projects/drawing-board' name='Drawing Board' technologies='JavaSE, Swing' />
 
-            <ProjectLink link='/projects/chess-board' name='Chess Board' technologies='JavaSE' />
-            <br />
+                <ProjectLink link='/projects/chess-board' name='Chess Board' technologies='JavaSE' />
+            </div>
         </section>
     )
 }
