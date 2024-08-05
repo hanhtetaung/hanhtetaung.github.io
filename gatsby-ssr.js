d3e02ac96555e517aaed3355e08df714f35cf403
Han Htet Aung <juzbird11@gmail.com>
2024-08-05 20:29:16 +0700

    add wrapPageElement in gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,3 +2,7 @@
 export const onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: "en" });
 };
+
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
