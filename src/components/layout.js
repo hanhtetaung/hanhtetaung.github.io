40149bfcb67991208fc1812e35933db7bfaf39fe
Han Htet Aung <juzbird11@gmail.com>
2024-08-29 17:22:45 +0700

    add gradient background

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
         <main>
             <html lang="en" />
             <title>Portfolio</title>
+            <meta name="theme-color" content="#e3eeff" />
             <Navbar/>
             <div className='container mx-auto px-4 py-10'>
                 {children}
