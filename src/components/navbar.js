762b612196d38cd31acfdd5886b7da60abca4b5a
Han Htet Aung <juzbird11@gmail.com>
2024-08-29 15:15:57 +0700

    change background color

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -37,7 +37,7 @@ export default function NavbarDefault() {
   );
  
   return (
-      <Navbar className="mx-auto rounded-none max-w-full px-4 py-2 lg:px-8 lg:py-4 bg-inherit backdrop-filter-none">
+      <Navbar className="mx-auto rounded-none max-w-full px-4 py-2 lg:px-8 lg:py-4 bg-inherit backdrop-filter-none border-none">
         <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
             <Link to="/" className="flex items-center">
               <img src={Logo} className="w-[40px]" alt="Bird-logo" />
