df3b9a873ee89c6a9ce181d03c9a63207a9916b0
Han Htet Aung <juzbird11@gmail.com>
2024-09-01 11:31:50 +0700

    add unsplash contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,7 @@
 import Githublogo from '../../imgs/github-light-logo.png'
 import Linkedinlogo from '../../imgs/LI-In-logo.png'
 import Leetcodelogo from '../../imgs/Leetcode-logo.png'
+import Unsplashlogo from '../../imgs/unsplash-logo.png'
 
 import ContactIconButton from "./contact-icon-button";
 
@@ -10,7 +11,8 @@ export default function IconButtonRounded() {
     <div className="flex gap-4">
       <ContactIconButton link={"https://github.com/hanhtetaung"} alt={"Github"} img={Githublogo} />
       <ContactIconButton link={"https://www.linkedin.com/in/han-htet-aung-b2764523a/"} alt={"Linkedin"} img={Linkedinlogo} />
-      <ContactIconButton link={"https://leetcode.com/u/juzbird/"} alt={"Leetcode"} img={Leetcodelogo} />
+      <ContactIconButton link={"https://leetcode.com/u/hanhtetaung/"} alt={"Leetcode"} img={Leetcodelogo} />
+      <ContactIconButton link={"https://unsplash.com/@hanhtetaung"} alt={"Unsplash"} img={Unsplashlogo} />
   </div>
   );
 }
