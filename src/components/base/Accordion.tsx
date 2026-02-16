e0a8b987566bc93b1cacb665fd2ac70e1e42c59a
Han <juzbird11@gmail.com>
2026-02-16 11:49:02 +0700

    Refactor Accordion and ScrollArea components for improved class handling

--- src/components/base/Accordion.tsx
+++ src/components/base/Accordion.tsx
@@ -10,12 +10,12 @@ export function AccordionItem({ title, children }: AccordionItemProps) {
 	return (
 		<BaseAccordion.Item className="border-b border-gray-200">
 			<BaseAccordion.Header>
-				<BaseAccordion.Trigger className="group relative flex w-full items-baseline justify-between gap-4 py-2 pr-1 pl-3 text-left font-medium hover:bg-gray-100 focus-visible:z-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800 text-2xl">
+				<BaseAccordion.Trigger className="group relative flex w-full items-baseline justify-between gap-4 py-2 pr-1 pl-3 text-left font-medium hover:bg-gray-100 focus-visible:z-1 focus-visible:outline focus-visible:outline-blue-800 text-2xl">
 					{title}
 					<ChevronIcon className="mr-2 size-3 transition-all ease-out group-data-panel-open:-rotate-90 rotate-90" />
 				</BaseAccordion.Trigger>
 			</BaseAccordion.Header>
-			<BaseAccordion.Panel className="h-[var(--accordion-panel-height)] overflow-hidden text-base text-gray-600 transition-[height] ease-out data-[ending-style]:h-0 data-[starting-style]:h-0">
+			<BaseAccordion.Panel className="h-(--accordion-panel-height) overflow-hidden text-base text-gray-600 transition-[height] ease-out data-ending-style:h-0 data-starting-style:h-0">
 				<div className="flex justify-between gap-4 p-4">{children}</div>
 			</BaseAccordion.Panel>
 		</BaseAccordion.Item>
