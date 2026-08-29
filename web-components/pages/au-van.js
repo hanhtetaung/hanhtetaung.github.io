7e23345e5afa512474cbe6858fa44fa004174456
Han <juzbird11@gmail.com>
2026-08-29 14:55:40 +0700

    cleaning part 2

--- web-components/pages/au-van.js
+++
@@ -1,7 +0,0 @@
-import "../fonts.js";
-import "../components/app-frame.js";
-
-import "../sections/header.js";
-import "../sections/footer.js";
-
-import "../sections/au-van/intro.js";
--- web-components/pages/thank-you.js
+++
@@ -1,6 +0,0 @@
-import "../fonts.js";
-
-import "../sections/header.js";
-import "../sections/footer.js";
-
-import "../sections/thank-you-intro.js";
